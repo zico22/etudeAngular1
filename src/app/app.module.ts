13a0db8e992e12b0304d1906f7afb3938c3697cd
marc <marc.cozic@agir-recouvrement.com>
2018-07-24 16:17:29 +0200

    debut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { AppRoutesModules } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, PageNotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, DashboardModule, AppRoutesModules],
   providers: [],
   bootstrap: [AppComponent]
 })
